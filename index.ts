a372a161da488658c3d427f84a8a9d4be7ae888c
James Lemire <webdev.jameslemire@gmail.com>
2018-10-22 11:17:00 -0600

    JL master - Turn starting server into an async operation.

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
 // Server Dependencies
+require('dotenv').config({ path: `${__dirname}/../.env` })
 import express from 'express'
 import morgan from 'morgan'
+import massive from 'massive'
 
 // API Routes
 import app_router from './routes'
+
+// Environment Variables
+const { NODE_ENV, PORT, DB_HOST, DB_DATABASE, DB_USER, DB_PASS } = process.env
 
 // Express App
 const app = express()
@@ -17,5 +22,26 @@ app.use(morgan('dev'))
 app.use('/api', app_router)
 
 // Start the server
-const PORT: number = 4020
-app.listen(PORT, () => console.log(`Sever listening on port ${PORT}!`))
+async function startServer() {
+  try {
+    const db_instance = await massive(
+      {
+        host: DB_HOST,
+        database: DB_DATABASE,
+        user: DB_USER,
+        password: DB_PASS
+      },
+      { scripts: `${__dirname}/../db` }
+    )
+
+    app.set('db', db_instance)
+
+    console.log(`Successfully connected to database ${DB_DATABASE}`)
+    console.log(`Server running on NODE_ENV ${NODE_ENV}`)
+
+    app.listen(PORT, () => console.log(`Sever listening on port ${PORT}!`))
+  } catch (err) {
+    console.error('startServer failed in index.ts:', err)
+  }
+}
+startServer()
