d1ed121b9090db4b6ad1e45ad172820ab104843f
James Lemire <webdev.jameslemire@gmail.com>
2018-10-22 12:06:27 -0600

    JL master - Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,6 @@
-DROP TABLE users;
-DROP TABLE shelfs;
 DROP TABLE books;
+CREATE TABLE books (book_id SERIAL PRIMARY KEY, name TEXT NOT NULL, created_at TIMESTAMP DEFAULT NOW());
 
-CREATE TABLE users (user_id SERIAL PRIMARY KEY);
-CREATE TABLE shelfs (shelf_id SERIAL PRIMARY KEY, user_id INT REFERENCES users(user_id) ON DELETE CASCADE);
-CREATE TABLE books (book_id SERIAL PRIMARY KEY, shelf_id INT REFERENCES shelfs(shelf_id) ON DELETE CASCADE, name TEXT NOT NULL);
-
+INSERT INTO books (name) VALUES
+('My Greatest Adventure'),
+('My Boringest Adventure');
